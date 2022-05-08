746a68b3406772658b1bdb329e61dd8e15877a49
brisi <brisiduri@gmail.com>
2022-05-08 19:16:18 +0200

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,168 @@
+const logotext = "Brisi";
+const meta = {
+    title: "Brisida Duri",
+    description: "I’m Brisida Duri Web Developer",
+};
+
+const introdata = {
+    title: "I’m Brisida Duri",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://scontent.ftia2-1.fna.fbcdn.net/v/t39.30808-6/280404953_10224068276393570_6619992637476973725_n.jpg?_nc_cat=109&ccb=1-6&_nc_sid=730e14&_nc_ohc=7417gpKM_REAX9SC0v5&_nc_ht=scontent.ftia2-1.fna&oh=00_AT_vNv-a2T1xBnRqmYUbPYWK1XH2PGvXx4P3nLzVosA1ow&oe=627D0188",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "Regulary learning new things and improving my self.Passionate about art and creativity.",
+};
+const worktimeline = [{
+        jobtitle: "Electrical Engineer",
+        where: "OSHEE",
+        date: "2014-Now",
+    },
+    {
+        jobtitle: "Electrical Engineer",
+        where: "NEC Albania",
+        date: "2014-2015",
+    },
+    {
+        jobtitle: "Event Planner",
+        where: "Clover Events",
+        date: "2019-Now",
+    },
+];
+
+const skills = [{
+        name: "AutoCad",
+        value: 60,
+    },
+    {
+        name: "Wix",
+        value: 90,
+    },
+    {
+        name: "Microsoft Oficce",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 50,
+    },
+    {
+        name: "Html",
+        value: 70,
+    },
+    {
+        name: "CSS",
+        value: 60,
+
+    },
+];
+
+const services = [{
+        title: "Web Developer",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Electrical Projects",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wix Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "brisiduri@gmail.com",
+    YOUR_FONE: "(355)0695228407",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/BrisiAB",
+    facebook: "https://www.facebook.com/brisi.duri",
+    linkedin: "https://al.linkedin.com/in/brisida-duri-1b565a82?original_referer=https%3A%2F%2Fsearch.yahoo.com%2F",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
